48d0c3a7662396ffc6bba3c04c9ef58f5e8df12e
harloch <Dustin.Allen@harloch.com>
2017-03-17 20:04:01 -0500

    Remove Bulky Pods

--- Pods/Target Support Files/Pods-Postnatal Depression/Pods-Postnatal Depression-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Postnatal_Depression : NSObject
-@end
-@implementation PodsDummy_Pods_Postnatal_Depression
-@end
--- Pods/Target Support Files/Pods-Postnatal Depression/Pods-Postnatal Depression-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Postnatal_DepressionVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Postnatal_DepressionVersionString[];
-
